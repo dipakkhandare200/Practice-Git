78fb1d3953fa500c4095859531936cecb46ba25f
dipakkhandare200 <dipakkhandare200@gmail.com>
2023-12-04 21:51:04 +0530

    Adding js file

---
+++ leet.js
@@ -0,0 +1,28 @@
+
+<div class="btn-toolbar">
+    <div class="btn-group"></div>
+    <div class="btn-group"></div>
+    <div class="btn-group"></div>
+</div>
+
+<<div class="btn-toolbar" role="toolbar" aria-label="Toolbar">
+    <div class="btn-group" role="group" aria-label="Button Group">
+        <button type="button" class="btn btn-primary">Button</button>
+        <button type="button" class="btn btn-primary">Button</button>
+        <button type="button" class="btn btn-primary">Button</button>
+    </div>
+    <div class="btn-group ms-3" role="group" aria-label="">
+        <button type="button" class="btn btn-primary">Button</button>
+        <button type="button" class="btn btn-primary">Button</button>
+    </div>
+</div> type="button" class="btn btn-large btn-block btn-danger">button</<div class="btn-toolbar" role="toolbar" aria-label="Toolbar">
+    <div class="btn-group" role="group" aria-label="Button Group">
+        <button type="button" class="btn btn-primary">Button</button>
+        <button type="button" class="btn btn-primary">Button</button>
+        <button type="button" class="btn btn-primary">Button</button>
+    </div>
+    <div class="btn-group ms-3" role="group" aria-label="">
+        <button type="button" class="btn btn-primary">Button</button>
+        <button type="button" class="btn btn-primary">Button</button>
+    </div>
+</div>>
